175ed0b0d7c41306f053810b4928594e86e49529
Angcroft <svenegas533@gmail.com>
2024-08-18 22:55:03 -0400

    Added a loadContent logic

--- src/com/quicknotes/controller/NoteController.java
+++ src/com/quicknotes/controller/NoteController.java
@@ -1,10 +1,14 @@
 package com.quicknotes.controller;
 
 import com.quicknotes.model.Note;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+
 
 //  Managing saving logic in text file
 public class NoteController {
@@ -18,5 +22,23 @@ public class NoteController {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public void loadNote(Note note) throws IOException{
+        final String fileName = "notes.txt";
+        final Path path = Paths.get(fileName);
+
+        StringBuilder content = new StringBuilder();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                content.append(line).append("\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        note.setContent(content.toString());
     }
 }
